394cc7196a286020cc10e71cb4117c59c394dd23
Ben Stein <bumstain@gmail.com>
2014-10-25 21:36:26 +0100

    Added validation check for user input

--- calculator.rb
+++ calculator.rb
@@ -3,44 +3,70 @@ def say(message)
 	puts "=> #{message}"
 end
 
-# Validate to not be zero
-def divide_by_zero
-	while operator == 'divide'
-		if n2 == 0
-		puts "You cannot divide by zero. Please enter another number."
-		else n2 == gets.chomp
-		end
-	end
+
+# Check user input is a valid number
+def check_num(i)
+	i.to_i.to_s == i || i.to_f.to_s == i
 end
 
+def user_input 
+	begin
+		input = gets.chomp
+		puts "That is not a valid input. Please enter a number." unless check_num(input)
+	end until check_num(input)
+	return input
+end
+
+
+# Request input from user
 say "Enter your first number."
-n1 = gets.chomp
-
-
-say "Enter your second number."
-n2 = gets.chomp
+n1 = user_input
 
 say "Would you like to add, subtract, multiply or divide?"
 operator = gets.chomp
 
+say "Enter your second number."
+n2 = user_input
 
 
 # Use conditions and operator to perform calculation function
-if operator == 'add'
-	result = n1.to_f + n2.to_f
-	operator = "plus"
-elsif operator == 'subtract'
-	result = n1.to_f - n2.to_f
-	operator = "minus"
-elsif operator == 'multiply'
-	result = n1.to_f * n2.to_f
-	operator = "multiplied by"
-else operator == 'divide'
-	divide_by_zero(n2)
-	result = n1.to_f / n2.to_f
-	operator = "divided by"
+case
+	when operator == 'add'
+		result = n1.to_f + n2.to_f
+		operator = "plus"
+	when operator == 'subtract'
+		result = n1.to_f - n2.to_f
+		operator = "minus"
+	when operator == 'multiply'
+		result = n1.to_f * n2.to_f
+		operator = "multiplied by"
+	when operator == 'divide'
+		result = n1.to_f / n2.to_f
+		operator = "divided by"
+	else
+		puts 
 end
+
 
 # Output result
 puts n1 + " " + operator + " " + n2 + " " + "= " "#{result}"
+
+
+
+
+
+
+
+# Check operator is a valid operator
+# def check_op(operator)
+# 	operator == add || subtract || multiply || divide 
+# end
+
+# Get a valid operator from user
+# def operator_check
+# 	begin
+# 		input = gets.chomp
+# 		puts "That is not a valid function for this program. Please choose add, subtract, multiply or divide." 
+# 	end
+# end
 
