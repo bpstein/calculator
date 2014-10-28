0c3421ae618fb9dc939341ce3b4ff2840f0e965a
Ben Stein <bumstain@gmail.com>
2014-10-28 18:48:56 +0000

    Minor edits and fixed indentation

--- calculator.rb
+++ calculator.rb
@@ -1,19 +1,19 @@
 # Request user input
 def say(message)
-	puts "=> #{message}"
+  puts "=> #{message}"
 end
 
 # Check user's numerical input is a valid number
 def check_num(i)
-	i.to_i.to_s == i || i.to_f.to_s == i
+  i.to_i.to_s == i || i.to_f.to_s == i
 end
 
 def user_input 
-	begin
-		input = gets.chomp
-		puts "That is not a valid input. Please enter a number." unless check_num(input)
-	end until check_num(input)
-	return input
+  begin
+    input = gets.chomp
+    puts "That is not a valid input. Please enter a number." unless check_num(input)
+  end until check_num(input)
+  return input
 end
 
 # Request first numerical input from user
@@ -27,20 +27,20 @@ operator_check = false
 
 # Check that user's requested operator is a valid operator 
 while operator_check == false 
-	if operator == 'add'
-	break
-	elsif operator == 'subtract'
-	break
-	elsif operator == 'multiply'
-	break
-	elsif operator == 'divide'
-	break
-	# Notify user that the requested operator is invalid
-	else
-		puts "Sorry, that is not a valid option for this program. Please choose add, subtract, multiply or divide." 
-		operator = gets.chomp
-		operator_check == false
-	end
+  if operator == 'add'
+  break
+  elsif operator == 'subtract'
+  break
+  elsif operator == 'multiply'
+  break
+  elsif operator == 'divide'
+  break
+  # Notify user that the requested operator is invalid
+  else
+    puts "Sorry, that is not a valid option for this program. Please choose add, subtract, multiply or divide." 
+    operator = gets.chomp
+    operator_check == false
+  end
 end
 
 # Request second numerical input from user
@@ -49,19 +49,19 @@ n2 = user_input
 
 # Use conditions and operator to perform calculation function
 case operator
-	when 'add'
-		result = n1.to_f + n2.to_f
-		operator = "plus"
-	when 'subtract'
-		result = n1.to_f - n2.to_f
-		operator = "minus"
-	when 'multiply'
-		result = n1.to_f * n2.to_f
-		operator = "multiplied by"
-	when 'divide'
-		result = n1.to_f / n2.to_f
-		operator = "divided by"
-	end
+  when 'add'
+    result = n1.to_f + n2.to_f
+    operator = "plus"
+  when 'subtract'
+    result = n1.to_f - n2.to_f
+    operator = "minus"
+  when 'multiply'
+    result = n1.to_f * n2.to_f
+    operator = "multiplied by"
+  when 'divide'
+    result = n1.to_f / n2.to_f
+    operator = "divided by"
+  end
 
 # Output result
 puts n1 + " " + operator + " " + n2 + " " + "= " "#{result}"
